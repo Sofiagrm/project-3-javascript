30eb652f701ee16916d80156b98fea5af6ca3c22
Sofia Moreira <smoreira@compass-group.fr>
2024-03-07 13:53:32 +0000

    first commit

---
+++ moreira_sofia_test_javascript_algo_ex1.js
@@ -0,0 +1,37 @@
+
+window.onload = () => {
+
+    const to_verify = new Array(9);
+
+
+    const F11 = () => {
+        for (let i = 0; i<= 8 ; i++) {
+            let arr1 = array_number[i].split(' ');
+            arr1.forEach((v, i, a) => a[i] = parseInt(v));
+            to_verify[i] = arr1;
+        }
+    };
+
+    const F12 = () => {
+        to_verify.forEach(line => {
+            let table_line = document.createElement('div');
+            table_line.style.width = '200px';
+            table_line.style.height = '20px';
+            table_line.style.display = 'flex';
+            table_line.style.flexDirection = 'row';
+            
+            line.forEach(column_value => {
+                let table_cell = document.createElement('div');
+                table_cell.style.width = '20px';
+                table_cell.style.height = '20px';
+                table_cell.style.border = '1px solid black';
+                table_cell.style.textAlign= 'center';
+                table_cell.style.align = 'middle';
+                table_cell.innerHTML = column_value;
+                table_line.append(table_cell);
+            });
+    
+            document.body.append(table_line);
+        });
+    };
+};
---
+++ moreira_sofia_test_javascript_algo_ex2.js
@@ -0,0 +1,12 @@
+window.onload = () => {
+
+    const F12 = (A) => {
+        console.log(A);
+        if(A.length !== 9) {
+            alert('not a list of 9 elements!')
+        }
+        else {
+            return A.every(v => A.indexOf(v) === A.lastIndexOf(v));
+        }
+    } 
+};
